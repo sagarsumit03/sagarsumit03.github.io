cbb3db86c19a366607cbceb052b53a3a1a6d65b3
Sumit Sagar <sagarsumit03@gmail.com>
2016-08-22 12:56:26 +0530

    Added Insertion sort, selection sort and mergeSort

--- src/com/sumit/algorithm/SortingAlgo.java
+++ src/com/sumit/algorithm/SortingAlgo.java
@@ -65,24 +65,163 @@ public class SortingAlgo {
 	public void insertionSort(int[] arr) {
 		int i = 1;
 		int j = i - 1;
-		int k =0;
+		int k = 0;
 		while (i < arr.length) {
-			k=i;
-			System.out.println("the init value of i is"+i);
+			k = i;
 			while (j >= 0) {
-				System.out.println("the init value of j is"+j);
 				if (arr[j] > arr[i]) {
 					swap(arr, j, i);
-					System.out.println("after swap value at "+i+" is "+arr[i]);
-					System.out.println("after swap value of "+j+" is "+arr[j]);
 				}
 				j--;
 				i--;
 			}
-			i=k+1;
+			i = k + 1;
 			j = i - 1;
 		}
 		System.out.println(Arrays.toString(arr));
 
 	}
+
+	public int searchMin(int index1, int index2, int arr[]) {
+		int min = arr[index1];
+		int i = 0;
+		int counter = index1;
+		while (index1 <= index2) {
+			if (arr[index1] < min) {
+				counter = index1;
+				min = arr[index1];
+				index1++;
+			} else {
+				index1++;
+			}
+		}
+
+		// System.out.println(i);
+		System.out.println(counter);
+		return counter;
+	}
+
+	public void selectionSort(int arr[]) {
+		System.out.println("abc" + Arrays.toString(arr));
+		int i = 0;
+		int index = 0;
+		while (i < arr.length) {
+			index = searchMin(i, arr.length - 1, arr);
+			System.out.println("in" + arr[index]);
+			swap(arr, i, index);
+			i++;
+		}
+		System.out.println("abc" + Arrays.toString(arr));
+	}
+
+	public void quickSort(int arr[]) {
+		int currentElement = 0;
+		int i = 0;
+		int pivot = 0;
+		int pivotIndex = arr.length - 1;
+		int sortedIndex = 0;
+		while (i < arr.length) {
+			System.out.println("i " + i);
+			pivot = arr[pivotIndex];
+			System.out.println(" pivot " + pivot);
+			if (arr[currentElement] < pivot) {
+				swap(arr, currentElement, sortedIndex);
+				sortedIndex++;
+				System.out.println("sortedIndex " + sortedIndex);
+			}
+			if (currentElement == pivotIndex) {
+				swap(arr, pivotIndex, sortedIndex);
+				sortedIndex++;
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public int[] mergeArrays(int[] arr1, int[] arr2) {
+		int[] arr = new int[arr1.length + arr2.length];
+		int i = 0;
+		int k = 0;
+		int j = 0;
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i] < arr2[j]) {
+				arr[k] = arr1[i];
+				i++;
+			} else {
+				arr[k] = arr2[j];
+				j++;
+			}
+			k++;
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		while (i < arr1.length)
+	    {
+	        arr[k] = arr1[i];
+	        i++;
+	        k++;
+	    }
+
+	    while (j < arr2.length)
+	    {
+	        arr[k] = arr2[j];
+	        j++;
+	        k++;
+	    }
+		
+		return arr;
+
+	}
+	
+	
+	
+	public int[] merge(int[] arr, int i, int j, int k){
+		//where i is the init index, j is the last index for sorted array, and j+1 to k is
+		//the last index.
+		int l =i;
+		int n =j+1;
+		int m=0;
+		int newArr[] = new int[arr.length];
+		while(l<=j && n<=k){
+			if(arr[i]<arr[n]){
+				newArr[m]=arr[i];
+				i++;
+			}else if(arr[i]>arr[n]){
+				newArr[m]=arr[n];
+				n++;
+			}
+			m++;
+		}
+		m=m-1;
+		System.out.println(" val of i"+i);
+		System.out.println("val of j "+j);
+		System.out.println(" val of m "+m);
+		while (l <= j)
+	    {
+	        arr[m] = arr[l];
+	        l++;
+	        m++;
+	    }
+
+	    while (n <= k)
+	    {
+	        arr[m] = arr[n];
+	        n++;
+	        m++;
+	    }
+		System.out.println(Arrays.toString(newArr));
+		return newArr;
+	}
+
+	public void mergeSort(int arr[], int lowIndex, int highIndex) {
+		int mid=0;
+		if(lowIndex<highIndex){
+			mid=(lowIndex+highIndex)/2;
+			mergeSort(arr, lowIndex, mid);
+			mergeSort(arr, mid+1, highIndex);
+			merge(arr, lowIndex, mid, highIndex);
+		}
+		System.out.println(" The list is "+Arrays.toString(arr));
+	}
+
 }
--- src/com/sumit/algorithm/SortingAlogTest.java
+++ src/com/sumit/algorithm/SortingAlogTest.java
@@ -1,13 +1,20 @@
 package com.sumit.algorithm;
 
+import java.util.Arrays;
+
 public class SortingAlogTest {
 
 	public static void main(String[] args) {
 
 		SortingAlgo sorted = new SortingAlgo();
-		int[] unsortedArr = new int[]{5,6,8,2,7,3,1,4};
-		//sorted.bubbleSort(unsortedArr);
-		sorted.insertionSort(unsortedArr);
+		int[] unsortedArr = new int[] { 64, 25, 12, 22, 11 };
+		int[] sortedArr = new int[] {12,25, 64, 11, 22 };
+		// sorted.bubbleSort(unsortedArr);
+		 //sorted.insertionSort(unsortedArr);
+		//sorted.selectionSort(unsortedArr);
+		//sorted.quickSort(unsortedArr);
+		sorted.merge(sortedArr, 0, 2, 4);
+		//sorted.mergeSort(unsortedArr, 0, unsortedArr.length-1);
 	}
 
 }
